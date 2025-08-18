96467f11954519f76cdcb0c2d4a9304c93fe85bc
RZFeeser <rzfeeser@users.noreply.github.com>
2025-08-18 19:43:27 -0400

    Update order.yml
    Create order.py
    
    Update README.md
    
    Create bug_report.md
    
    Create order.yml
    
    Update order.py
    
    Update order.yml
    
    Create requests.txt
    
    Update order.yml
    
    Update order.py

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # githubActions-OrderADominosPizza
-Using Firefox and Selenium we can order a Dominos Pizza using GitHub Actions
+Hey gang! Professor Feeser here. This repository lets you use GitHub actions, Firefox and Selenium to order a Dominos Pizza! In order to make this script work, you'll need to set the following GitHub secrets:
+- `secrets.EMAIL` - Email address to attach to your order
+- `secrets.PHONE` - Phone number to associate with your Dominos order
+- `secrets.FIRST_NAME` - First name on your Dominos order
+- `secrets.LAST_NAME` - Last name on your Dominos order
+- `secrets.CITY` - City you order Dominos Pizza from
+- `secrets.STATE` - Two letter state abbrivation
+
+
+## Structure
+### /
+  - **order.py** - This is the script that does the ordering. The order itself is paid for in cash, therefore, for security, the final "order" click is commented out. You need to uncomment this yourself to make the script work correctly.
+
+### /.github/workflows
+  - **order.yml** - A GitHub workflow that uses Selenium to order the pizza.
+  
+
+## Video Demos
+Videos demonstrating the code in this repository are available on the author's [YouTube Channel @CodeWithFeeser](https://www.youtube.com/@CodeWithFeeser):  
+
+- [YouTube CodeWithFeeser - Learn GitHub Actions - Order a Dominos Pizza]()
+- [YouTube CodeWithFeeser - Learn Python - Order a Dominos Pizza with Selenium]()
+
+
+If you found a video helpful, be sure to hit **like** and **subscribe** for weekly lessons from [YouTube Channel @CodeWithFeeser](https://www.youtube.com/@CodeWithFeeser)
+
+
+## How to Use This Repository
+This repository is a best practice repository I use for teaching structure and organization of Selenium and GitHub actions. If you have any suggestions, feel free to open an issue. If you haven't already, be sure to watch the videos I have posted on using the content found in this repository.
+
+
+## Helpful Notes
+- 
+
+
+## Help & Training
+Since 2010 I've been providing in person and online technical training solutions for large organizations and individuals. If you're looking for a training solution, reach out via my [contact portal at IRIS7](https://iris7.com/contact)
+
---
+++ order.py
@@ -0,0 +1,144 @@
+#!/usr/bin/python3
+"""Author RZFeeser for youtube.com/@CodeWithFeeser
+  
+   NOTE: This workflow ACTUALLY ORDERS A PIZZA FROM DOMINOS. In no way you should be using this script maliciously, only deliciously. For safety, I have the final order click commented out of `order.py`. Unless you intend or purhcasing a pizza, do not uncomment this last line. Ultimately, I wrote it as a tool for teaching how one could use GitHub Actions to do just about anything (like order a pizza), as well as creating a fun oppertunity to teach about Selenium testing. 
+
+   Objective:
+   Place an order for a Domino's Pizza using Selenium & Firefox. I'd like to adapt this into a GitHub workflow, so that anyone can order from Domino's using GitHub.
+
+   Remarks:
+   - I've been running this exercise for a few years. The Domino's Web API has been surprisingly stable (i.e. I have
+   not needed to update any x-paths).
+   - Camelot, 'tis a silly place.
+
+   Environmental Variables
+   CITY - City to order pizza from
+   STATE - Two letter state abbreviation
+   LAST_NAME - 
+   FIRST_NAME -
+   EMAIL -
+   PHONE -
+
+   At this time I have Hardcoded:
+   Carryout & Cheese, but there is not anything that isn't easily converted to ENV inputs.
+
+   GitHub:
+
+   z@iris7.com
+   """
+
+
+# standard library
+import os
+import time
+
+# 3rd party libraries
+# python -m pip install selenium
+from selenium import webdriver
+# from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+# capture environmental vars that control how the script runs
+for var in ["CITY", "STATE", "LAST_NAME", "FIRST_NAME", "EMAIL", "PHONE"]:
+    if var in os.environ:
+        globals()[var] = os.environ[var]
+
+# example...
+#if os.environ['CITY']:
+#    CITY = os.environ['CITY']
+
+def main():
+    """interaction with Selenium and Firefox"""
+
+    try:
+        # Open Firefox to Dominos.com
+        driver = webdriver.Firefox()
+        driver.get("https://www.dominos.com/en/")
+        time.sleep(5)
+
+        elem = driver.find_element(By.XPATH, "/html/body/div[1]/main/section/div/div/a[2]")
+        elem.click()
+        time.sleep(5)
+
+        # Select Find Store
+        elem = driver.find_element(By.XPATH, "//*[@id='City']")
+        #elem = driver.find_element(By.XPATH, "//*[contains(text(), 'City')]")
+
+        # The city and state should be updated with envs
+        elem.send_keys(CITY)
+        elem = driver.find_element(By.XPATH, "//*[@id='Region']")
+        elem.send_keys(STATE)
+        elem = driver.find_element(By.XPATH, "/html/body/div[1]/main/section/article/div/div[2]/div[2]/form/div/button")
+        elem.click()
+        time.sleep(5)
+
+        #click on CarryOut - //*[@id="Service_Method_Carryout"]
+        elem = driver.find_element(By.XPATH, "/html/body/div[3]/div[3]/div/div/div/div[2]/div[1]/div[3]/div/div[2]/div[2]/a")
+        elem.click()
+        time.sleep(5)
+
+        #click on Curbside Delivery <- if you choose pay by cash this is not an option
+        #    elem = driver.find_element(By.XPATH, "/html/body/div[3]/div[3]/div/div/div/div[2]/div[27]/div[3]/div/div[2]/div[1]/a/span[1]")
+        #    elem.click()
+        #    time.sleep(5)
+
+        #click on Pizza Builder
+        elem = driver.find_element(By.XPATH,"/html/body/div[3]/div[2]/div/div/div[2]/div[6]/a")
+        elem.click()
+        time.sleep(5)
+
+        #Add to Order
+        elem = driver.find_element(By.XPATH,"/html/body/div[5]/div/section/div/div/ol/li[8]/aside/section/div/div[3]/button")
+        elem.click()
+        time.sleep(3)
+
+        #Cheese it up... no
+        elem = driver.find_element(By.XPATH,"/html/body/div[5]/div/section/div/div/div/div/button[1]")
+        elem.click()
+        time.sleep(5)
+
+        #Checkout
+        driver.get('https://www.dominos.com/en/pages/order/payment')
+        time.sleep(5)
+
+        # Choose first name
+        elem = driver.find_element(By.XPATH, "//*[@id='First_Name']")
+        elem.send_keys(FIRST_NAME)
+        time.sleep(1)
+
+        # Choose last name
+        elem = driver.find_element(By.XPATH, "//*[@id='Last_Name']")
+        elem.send_keys(LAST_NAME)
+        time.sleep(1)
+
+        # Choose email address
+        elem = driver.find_element(By.XPATH, "//*[@id='Email']")
+        elem.send_keys(EMAIL)
+        time.sleep(1)
+
+        # Set callback phone
+        elem = driver.find_element(By.XPATH, "//*[@id='Callback_Phone']")
+        for number in PHONE: # for some reason numbers don't always copy and paste correctly
+            elem.send_keys(number) # this seems to be a decent fix
+            time.sleep(1)
+
+        # opt out of offers
+        elem = driver.find_element(By.XPATH, "//*[@id='EmailOptIn']")
+        elem.click()
+
+        # Opt out of Pizza Rewards
+        elem = driver.find_element(By.XPATH, "//*[@id='Loyalty_Enrollment_No']")
+        elem.click()
+
+        # pay cash at the store
+        elem = driver.find_element(By.XPATH, "//*[@id='Cash']")
+        elem.click()
+
+    # regardless if things worked or failed, grab a screenshot
+    finally:
+        time.sleep(5)
+        driver.save_screenshot("screenshot.png")
+
+# call main
+if __name__ == "__main__":
+    main()
