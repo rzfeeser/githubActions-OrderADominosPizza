ddf4587a399adaa6d82ed81d3938b60c4ceafc29
RZFeeser <rzfeeser@users.noreply.github.com>
2025-08-21 18:47:59 -0400

    Update order.py

--- order.py
+++ order.py
@@ -230,7 +230,7 @@ def main():
             #elem = driver.find_element(By.XPATH, "/html/body/div[1]/section/main/section/div[1]/div/form/section/section[1]/fieldset[1]/button/div/span")
             #elem.click()
 
-            driver.execute_script("window.scrollBy(0, 330);")
+            driver.execute_script("window.scrollBy(0, 800);")
             time.sleep(1)
             
             # Opt out of Pizza Rewards
